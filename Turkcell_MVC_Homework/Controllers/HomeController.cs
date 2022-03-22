070fa23161c5cd80d8fd00c3ac907a7684951fa1
Eray Nurtekin <79145084+eraynurtekin@users.noreply.github.com>
2022-03-22 11:41:34 +0300

    Add project files.

---
+++ Turkcell_MVC_Homework/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Turkcell_MVC_Homework.Models;
+
+namespace Turkcell_MVC_Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Volunteer volunter)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks");
+            }
+            return View("Error");
+        }
+        [HttpGet]
+        public IActionResult Exit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Turkcell_MVC_Homework/Models/Volunteer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Turkcell_MVC_Homework.Models
+{
+    public class Volunteer
+    {
+        [Required(ErrorMessage ="Lütfen İsminizi Giriniz:")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen Soyadınızı Giriniz:")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Lütfen Email Hesabınızı Giriniz:")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Lütfen Yaşınızı Giriniz:")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Lütfen Telefon No Giriniz:")]
+        public string Telephone { get; set; }  
+
+    }
+}
